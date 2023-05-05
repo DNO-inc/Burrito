80c3b3986a9787f1ff363e4cd5d26ee8c0fe9db8
moder <bogdan.chernenko.018@gmail.com>
2023-05-05 20:59:32 +0300

    [apps] apply new concept to the registration app

--- burrito/apps/registration/router.py
+++ burrito/apps/registration/router.py
@@ -1,11 +1,19 @@
 from fastapi import APIRouter
 
 from .views import (
-    registration_main,
+    RegistrationMainView,
     check_verification_code
 )
 
 registration_router = APIRouter()
 
-registration_router.add_api_route("/", registration_main, methods=["POST"])
-registration_router.add_api_route("/send_code", check_verification_code, methods=["POST"])
+registration_router.add_api_route(
+    "/",
+    RegistrationMainView.post,
+    methods=["POST"]
+)
+registration_router.add_api_route(
+    "/send_code",
+    check_verification_code,
+    methods=["POST"]
+)
--- burrito/apps/registration/views.py
+++ burrito/apps/registration/views.py
@@ -9,46 +9,53 @@ from .utils import (
     get_hash,
     create_user, get_user_by_login,
     is_valid_login, is_valid_password,
-    status
+    BaseView,
+    status,
+    check_permission
 )
 
 
-async def registration_main(user_data: UserPasswordLoginSchema):
-    """Handle user registration"""
+class RegistrationMainView(BaseView):
+    _permissions: list[str] = [""]
 
-    if not is_valid_login(user_data.login):
-        return JSONResponse(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            content={"detail": "Invalid login"}
+    @staticmethod
+    @check_permission
+    async def post(user_data: UserPasswordLoginSchema):
+        """Handle user registration"""
+
+        if not is_valid_login(user_data.login):
+            return JSONResponse(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                content={"detail": "Invalid login"}
+            )
+
+        if not is_valid_password(user_data.password):
+            return JSONResponse(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                content={"detail": "Invalid password"}
+            )
+
+        if get_user_by_login(user_data.login):
+            return JSONResponse(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                content={"detail": "User with the same login exist"}
+            )
+
+        user_id_value: int | None = create_user(
+            user_data.login,
+            get_hash(user_data.password)
         )
 
-    if not is_valid_password(user_data.password):
+        if user_id_value:
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content={"user_id": user_id_value}
+            )
+
         return JSONResponse(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            content={"detail": "Invalid password"}
+            status_code=status.HTTP_409_CONFLICT,
+            content={"detail": "'\\_(-_-)_/'"}
         )
-
-    if get_user_by_login(user_data.login):
-        return JSONResponse(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            content={"detail": "User with the same login exist"}
-        )
-
-    user_id_value: int | None = create_user(
-        user_data.login,
-        get_hash(user_data.password)
-    )
-
-    if user_id_value:
-        return JSONResponse(
-            status_code=status.HTTP_200_OK,
-            content={"user_id": user_id_value}
-        )
-
-    return JSONResponse(
-        status_code=status.HTTP_409_CONFLICT,
-        content={"detail": "'\\_(-_-)_/'"}
-    )
 
 
 async def check_verification_code(code_object: UserVerificationCode):
