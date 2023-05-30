7dc11249a2ef96fda6f04a23fbbcd4aff8cc8b86
moder <bogdan.chernenko.018@gmail.com>
2023-05-31 01:22:59 +0300

    replaced some function from 'db_utils' component to 'users_util'

--- burrito/utils/db_utils.py
+++ burrito/utils/db_utils.py
@@ -1,5 +1,3 @@
-from fastapi import HTTPException, status
-
 from burrito.models.tickets_model import Tickets
 from burrito.models.roles_model import Roles
 from burrito.models.statuses_model import Statuses
@@ -21,7 +19,6 @@ from burrito.models.bookmarks_model import Bookmarks
 from burrito.models.permissions_model import Permissions
 from burrito.models.role_permissions_model import RolePermissions
 
-from burrito.utils.converter import GroupStrToInt, FacultyStrToInt
 from burrito.utils.db_cursor_object import get_database_cursor
 from burrito.utils.logger import get_logger
 
@@ -80,100 +77,3 @@ def drop_tables(use: bool = False):
         )
     )
     get_logger().warning("All tables was dropped")
-
-
-def create_user_tmp_foo(
-    login: str, hashed_password: str,
-    group: str, faculty: str
-) -> Users | None:
-    """_summary_
-
-    Create user with default fields: (login, hashed_password)
-
-    Args:
-        login (str): user login
-        hashed_password (str): user hashed password
-
-    Returns:
-        bool: status creating new user
-    """
-
-    try:
-        group_id = GroupStrToInt.convert(group)
-        faculty_id = FacultyStrToInt.convert(faculty)
-
-        if not (group_id and faculty_id):
-            return
-
-        user: Users = Users.create(
-            login=login, password=hashed_password,
-            group=group_id,
-            faculty=faculty_id,
-            role=Roles.get(Roles.name == "ALL")
-        )
-        return user
-
-    except Exception as e:  # pylint: disable=broad-except, invalid-name
-        get_logger().error(e)
-
-
-# TODO: delete this function
-def create_user(login: str, hashed_password: str) -> int | None:
-    """_summary_
-
-    Create user with default fields: (login, hashed_password)
-
-    Args:
-        login (str): user login
-        hashed_password (str): user hashed password
-
-    Returns:
-        bool: status creating new user
-    """
-
-    try:
-        user: Users = Users.create(
-            login=login, password=hashed_password
-        )
-        return user.user_id
-
-    except Exception as e:  # pylint: disable=broad-except, invalid-name
-        get_logger().error(e)
-
-
-def get_user_by_login(login: str) -> Users | None:
-    """_summary_
-
-    Get user if exist or return None
-
-    Args:
-        login (str): user login
-
-    Returns:
-        Users | None: return None if user is not exist
-    """
-
-    return Users.get_or_none(Users.login == login)
-
-
-def get_user_by_id(user_id: int) -> Users | None:
-    """_summary_
-
-    Get user if exist or return None
-
-    Args:
-        user_id (int): user id
-
-    Returns:
-        Users | None: return None if user is not exist
-    """
-
-    _current_user = Users.get_or_none(Users.user_id == user_id)
-
-    if not _current_user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"User with ID ({user_id}) is not exist"
-        )
-
-    return _current_user
