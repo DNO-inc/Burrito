cbe89491c64c7af67ea890436d88132362b88bcf
m-o-d-e-r <bogdan.chernenko.018@gmail.com>
2023-07-11 20:07:15 +0300

    start preparation to use microservice architecture

---
+++ burrito/containers.py
@@ -0,0 +1,21 @@
+import uvicorn
+
+from burrito.utils.config_reader import get_config
+
+from burrito.init.init_system import InitManager
+from burrito.init.tasks.check_db_task import CheckDBTask
+from burrito.init.tasks.preprocessor_task import PreProcessorTask
+
+
+def prepare_app():
+    get_config()  # read configs
+
+    init_manager = InitManager(
+        error_attempt_delta=3
+    )
+    init_manager.add_task(CheckDBTask(attempt_count=100))
+    init_manager.add_task(PreProcessorTask(attempt_count=100))
+
+    init_manager.run_cycle()
+
+    return True
