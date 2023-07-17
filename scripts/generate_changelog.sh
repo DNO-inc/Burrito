d53b0a8c28bb76398203b13ca1a0e7a3ed722c98
m-o-d-e-r <bogdan.chernenko.018@gmail.com>
2023-07-17 21:10:03 +0300

    fix script for changelog generation

--- scripts/generate_changelog.sh
+++ scripts/generate_changelog.sh
@@ -1,2 +1,2 @@
 
-echo -e "\n\tGenerated $(date '+%F %T')\n\n" > CHANGELOG.md & git log --all --date=relative --pretty=format:"%h %x09 %ad %d %s (%aN)" >> CHANGELOG.md
+echo -e "\t==============================\n\tGenerated $(date '+%F %T')\n\t==============================\n\n" > CHANGELOG.md & git log --all --date=relative --pretty=format:"%x09 %h %ad %d %s (%aN)" >> CHANGELOG.md
