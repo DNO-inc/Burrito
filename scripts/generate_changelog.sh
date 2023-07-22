80f544669a0f234be3683dc661d4b35db33a40ee
m-o-d-e-r <bogdan.chernenko.018@gmail.com>
2023-07-22 22:26:38 +0300

    change version labels

--- scripts/generate_changelog.sh
+++ scripts/generate_changelog.sh
@@ -1,2 +1,4 @@
 
-echo -e "\t==============================\n\tGenerated $(date '+%F %T')\n\t==============================\n\n" > CHANGELOG.md & git log --all --date=relative --pretty=format:"%x09 %h %ad %d %s (%aN)" >> CHANGELOG.md
+echo -e "\t==============================\n\tGenerated $(date '+%F %T')\n\t==============================\n\n" > \
+    CHANGELOG.md & git log --all --date=relative --pretty=format:"%x09 %h %ad %d %s (%aN)" >> \
+    CHANGELOG.md
