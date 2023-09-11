d02ef70b2e088e41758df44416d0c4799b042d9d
m-o-d-e-r <bogdan.chernenko.018@gmail.com>
2023-09-11 12:10:05 +0300

    added timezone for scheduler

--- burrito/apps/scheduler/core.py
+++ burrito/apps/scheduler/core.py
@@ -4,8 +4,11 @@ import time
 from .preprocessor.core import preprocessor_task
 
 
+CURRENT_TZ = "Europe/Kyiv"
+
+
 def start_scheduler():
-    schedule.every().day.at("00:30").do(preprocessor_task)
+    schedule.every().day.at("00:30", CURRENT_TZ).do(preprocessor_task)
 
     while True:
         schedule.run_pending()
