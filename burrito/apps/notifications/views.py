5986bf50950b823a8e2827e56d1ab86c2ce00ed7
m-o-d-e-r <bogdan.chernenko.018@gmail.com>
2023-09-26 22:27:43 +0300

    hotfix

--- burrito/apps/notifications/views.py
+++ burrito/apps/notifications/views.py
@@ -1,7 +1,9 @@
 from fastapi import Depends
+from bson.objectid import ObjectId
 
 from burrito.models.m_notifications_model import NotificationMetaData, Notifications
 
+from burrito.utils.logger import get_logger
 from burrito.utils.mongo_util import mongo_select, mongo_delete, mongo_items_count
 from burrito.utils.auth import BurritoJWT, get_auth_core, AuthTokenPayload
 
@@ -10,6 +12,7 @@ async def notifications__get_notifications(__auth_obj: BurritoJWT = Depends(get_
     token_payload: AuthTokenPayload = await __auth_obj.require_access_token()
 
     notification_list = mongo_select(NotificationMetaData, user_id=token_payload.user_id)
+    output = mongo_select(Notifications, _id={"$in": [ObjectId(i["notification_id"]) for i in notification_list]})
 
     for item in notification_list:
         mongo_delete(NotificationMetaData, _id=item["_id"])
@@ -20,6 +23,8 @@ async def notifications__get_notifications(__auth_obj: BurritoJWT = Depends(get_
         ) == 0:
             mongo_delete(Notifications, _id=item["notification_id"])
 
+    get_logger().info(output)
+
     return {
-        "notifications": notification_list
+        "notifications": [Notifications(**i) for i in output]
     }
