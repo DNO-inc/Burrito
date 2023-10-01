6a3f046738c7d303fbf9cd1633da105b758febb8
m-o-d-e-r <bogdan.chernenko.018@gmail.com>
2023-10-02 01:31:35 +0300

    implement 'connection mode' (NOTIFICATIONS & CHAT)

--- burrito/apps/ws/utils.py
+++ burrito/apps/ws/utils.py
@@ -46,29 +46,7 @@ def recv_ping(websocket: WebSocketServerProtocol) -> None:
     get_logger().info(f"Pinging thread is finished {thread_id}")
 
 
-def main_handler(websocket: WebSocketServerProtocol):
-    thread_id = threading.get_native_id()
-    get_logger().info(f"New thread started: {thread_id}")
-
-    raw_data = recv_data(websocket)
-
-    if isinstance(raw_data, bytes):
-        raw_data = raw_data.decode("utf-8")
-
-    if not raw_data:
-        send_data(websocket, b"Auth fail")
-
-    token_payload: AuthTokenPayload = None
-    try:
-        token_payload = check_jwt_token(raw_data)
-
-    except:
-        send_data(websocket, b"Auth fail")
-        close_conn(websocket)
-        return
-
-    send_data(websocket, b"Auth OK")
-
+def notifications_cycle(websocket: WebSocketServerProtocol, token_payload: AuthTokenPayload):
     pubsub: PubSub = get_redis_connector().pubsub()
     pubsub.subscribe(f"user_{token_payload.user_id}")
 
@@ -92,6 +70,53 @@ def main_handler(websocket: WebSocketServerProtocol):
 
     except Exception as exc:
         get_logger().warning(f"{exc}")
+
+
+def chat_cycle(websocket: WebSocketServerProtocol, token_payload: AuthTokenPayload):
+    ...
+
+
+__CONNECTION_MODES = {
+    "NOTIFICATIONS": notifications_cycle,
+    "CHAT": chat_cycle
+}
+
+
+def main_handler(websocket: WebSocketServerProtocol):
+    thread_id = threading.get_native_id()
+    get_logger().info(f"New thread started: {thread_id}")
+
+    raw_data = recv_data(websocket)
+
+    if isinstance(raw_data, bytes):
+        raw_data = raw_data.decode("utf-8")
+
+    if not raw_data:
+        send_data(websocket, b"Auth fail")
+
+    token_payload: AuthTokenPayload = None
+    try:
+        token_payload = check_jwt_token(raw_data)
+
+    except:
+        send_data(websocket, b"Auth fail")
+        close_conn(websocket)
+        return
+
+    send_data(websocket, b"Auth OK")
+
+    connection_mode = recv_data(websocket)
+    if isinstance(connection_mode, bytes):
+        connection_mode = connection_mode.decode("utf-8")
+    if not connection_mode:
+        send_data(websocket, b"Connection mode is invalid")
+
+    connection_handler = __CONNECTION_MODES.get(connection_mode)
+    if connection_handler:
+        connection_handler(websocket, token_payload)
+    else:
+        send_data(websocket, b"Connection mode is invalid")
+        get_logger().warning(f"User {token_payload.user_id} has selected wrong connection mode '{connection_mode}'")
 
     get_logger().info(f"Thread {thread_id} is finished")
 
