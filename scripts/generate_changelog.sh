9f682a47dc38f20585a6787da769b87047b498d3
m-o-d-e-r <bogdan.chernenko.018@gmail.com>
2023-10-01 14:19:23 +0300

    update CHANGELOG.md generator

--- scripts/generate_changelog.sh
+++ scripts/generate_changelog.sh
@@ -1,4 +1,3 @@
 
 echo -e "\t============================================\n\tGenerated $(date '+%F %T\t%Z %z')\n\t============================================\n\n" > \
-    CHANGELOG.md & git log --all --date=relative --pretty=format:"%x09 %h %ad %d %s (%aN)" >> \
-    CHANGELOG.md
+    CHANGELOG.md & git log --all --date=relative --pretty=format:"%x09 %h %ad %d %s (%aN)" | grep -v "Merge pull" >> CHANGELOG.md
