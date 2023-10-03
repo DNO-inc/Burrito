5ca9be24de0f5869d28ae72963395c05514aac13
m-o-d-e-r <bogdan.chernenko.018@gmail.com>
2023-10-03 11:43:32 +0300

    added hierarchy to action classes

--- burrito/models/m_actions_model.py
+++ burrito/models/m_actions_model.py
@@ -4,28 +4,23 @@ from burrito.utils.date import get_datetime_now
 from burrito.models.m_basic_model import MongoBaseModel
 
 
-class Actions(MongoBaseModel):
+class BaseAction(MongoBaseModel):
     ticket_id: int
     user_id: int
     creation_date: str | object = Field(default_factory=get_datetime_now)
     field_name: str = Field(max_length=255)
+    type_: str = "action"
+
+    class Meta:
+        table_name: str = "ticket_history"
+        history_type: str = "action"
+
+
+class Actions(BaseAction):
     old_value: str = Field(max_length=255)
     new_value: str = Field(max_length=255)
-    type_: str = "action"
-
-    class Meta:
-        table_name: str = "ticket_history"
-        history_type: str = "action"
 
 
-class FileActions(MongoBaseModel):
-    ticket_id: int
-    user_id: int
-    creation_date: str | object = Field(default_factory=get_datetime_now)
-    field_name: str = "file"
+class FileActions(BaseAction):
+    field_name: str = Field(max_length=255, default="file")
     value: str = Field(max_length=255)
-    type_: str = "action"
-
-    class Meta:
-        table_name: str = "ticket_history"
-        history_type: str = "action"
