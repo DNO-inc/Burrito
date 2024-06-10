fac9de9f7095c8d88c14cb64da4ad359d2e11d22
m-o-d-e-r <moder.mode.on@gmail.com>
2024-06-10 14:48:08 +0300

    integrate Airflow into the project

---
+++ burrito/airflow/dags/preinstall_dag.py
@@ -0,0 +1,66 @@
+import datetime
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.models.baseoperator import chain
+from peewee import MySQLDatabase
+
+from burrito.utils.db_utils import create_tables
+from burrito.utils.db_cursor_object import get_database_cursor
+from burrito.utils.mongo_util import mongo_init_ttl_indexes
+from burrito.utils.logger import get_logger
+from burrito.airflow.utils import preprocessor_task
+
+from burrito.models.m_email_code import EmailVerificationCode
+from burrito.models.m_password_rest_model import AccessRenewMetaData
+
+
+def init_db_events():
+    with open(
+        "/opt/burrito_project/burrito/event_init.sql",
+        "r",
+        encoding="utf-8"
+    ) as file:
+        db: MySQLDatabase = get_database_cursor()
+
+        for query in file.read().split(";"):
+            query = query.replace('\t', "").replace("\n", "")
+            query = ' '.join(query.split())
+
+            if not query:
+                continue
+
+            try:
+                db.execute_sql(query)
+            except Exception as e:
+                get_logger().error(e)
+
+
+with DAG(
+    dag_id="preinstall_dag",
+    description="Preparing DB",
+    start_date=datetime.datetime.now() - datetime.timedelta(days=1),
+    schedule_interval=None,
+    is_paused_upon_creation=False
+) as dag:
+    chain(
+        PythonOperator(
+            task_id="create_db_tables",
+            python_callable=create_tables
+        ),
+        PythonOperator(
+            task_id="first_preprocessor_run",
+            python_callable=preprocessor_task
+        ),
+        PythonOperator(
+            task_id="create_mongo_ttl_indexes",
+            python_callable=mongo_init_ttl_indexes,
+            op_kwargs={
+                "models": [EmailVerificationCode, AccessRenewMetaData]
+            }
+        ),
+        PythonOperator(
+            task_id="create_db_events",
+            python_callable=init_db_events
+        )
+    )
